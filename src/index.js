8fb0796a90afc90d059c2dfae98db554c2f593b8
Kent Andersen <kent.andersen@bekk.no>
2021-06-11 13:48:37 +0200

    La til dynamisk lasting av interaktiv kode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-let number = window.location.hash || 10000
+function importCSS(href) {
+  var link = document.createElement('link');
 
+  link.href = href;
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
 
+  return new Promise((resolve, reject) => {
+    link.onload = resolve
+    link.onerror = reject
+    
+    document.head.appendChild(link);
+  })
+}
 
+Promise.all([
+  import('/src/interactive.js'),
+  importCSS('./src/interactive.js')
+])
+  .then(([module]) => {
+    console.log(module)
+  });
